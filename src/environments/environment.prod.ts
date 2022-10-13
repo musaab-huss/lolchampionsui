91b9e97b3c898a7580338fa1633f78b0891a5ee5
musaab-huss <63868165+musaab-huss@users.noreply.github.com>
2022-10-13 14:26:48 +0100

    Update environment.prod.ts
    updated the backend URL to test webhook

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://lol-champions-ose15-final-project.apps.ose.openshift.conygre.com/api/v1/lolchampion"
+  apiUrl: "https://lol-champions-backend-ose15-final-project.apps.ose.openshift.conygre.com//api/v1/lolchampion"
 };
